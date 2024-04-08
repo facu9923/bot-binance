df605088dc4e614a421e190f836094c68a7b6c8c
facu9923 <facundoarias1231@gmail.com>
2024-04-08 17:19:01 -0300

    acomodamiento de codigo

--- Bot-binance.py
+++ Bot-binance.py
@@ -11,7 +11,32 @@ def get_historic_dates(symbol, interval, fecha_inicial_str, fecha_actual_str):
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
     return df
 
+def getDataFrame():
+    data =pd.DataFrame( candles, columns=['open time', 'Open price', 'High price', 'Low price', 'Close', 'Volume', 'Close time', 'Asset volume', 'Number of trades', 'Taker volume', 'Maker volume', 'nothing'] )
 
+    data['Datetime'] = pd.DatetimeIndex(pd.to_datetime(data['Close time'], unit='ms'))
+    data['Open price'] = data['Open price'].astype('float')
+    data['High price'] = data['High price'].astype('float')
+    data['Low price'] = data['Low price'].astype('float')
+    data['Close'] = data['Close'].astype('float')
+    data['Volume'] = data['Volume'].astype('float')
+    data['Close time'] = pd.DatetimeIndex(pd.to_datetime(data['Close time'], unit='ms'))
+    data['Asset volume'] = data['Asset volume'].astype('float')
+    data['Number of trades'] = data['Number of trades'].astype('float')
+    data['Taker volume'] = data['Taker volume'].astype('float')
+    data['Maker volume'] = data['Maker volume'].astype('float')
+    data['nothing'] = data['nothing'].astype('float')
+
+    data = data.set_index('Datetime')
+
+    return data
+
+def run(saldo, ma, bb, rsi):
+    sobreventa = 50
+    sobrecompra = 20
+    while True:
+        print('ejec')
+        
 ################################# CONNECT ################################################
 key = 'pXybkp3SAJGjQD3IgrIF7C6QOB1IauVZB0InO2SBIczMZSyLmPqsTXp64piCgPcY'
 secret = 'BAJbHHjl9tofmxyrn416RvUgbG24gnC9UHsMpFqtuHRo54KBAbSGUtG9pOTDAZgUY'
@@ -31,31 +56,20 @@ fecha_actual_str = fecha_actual.strftime('%d %b, %Y')
 
 saldo = 100000.00
 
-hitoric_dates = get_historic_dates(symbol, interval, fecha_inicial_str, fecha_actual_str)
+##hitoric_dates = get_historic_dates(symbol, interval, fecha_inicial_str, fecha_actual_str)
 
 candles = client._historical_klines(symbol, interval)
 
-data =pd.DataFrame( candles, columns=['open time', 'Open price', 'High price', 'Low price', 'Close', 'Volume', 'Close time', 'Asset volume', 'Number of trades', 'Taker volume', 'Maker volume', 'nothing'] )
+df = getDataFrame()
 
-data['Datetime'] = pd.DatetimeIndex(pd.to_datetime(data['Close time'], unit='ms'))
-data['Open price'] = data['Open price'].astype('float')
-data['High price'] = data['High price'].astype('float')
-data['Low price'] = data['Low price'].astype('float')
-data['Close'] = data['Close'].astype('float')
-data['Volume'] = data['Volume'].astype('float')
-data['Close time'] = pd.DatetimeIndex(pd.to_datetime(data['Close time'], unit='ms'))
-data['Asset volume'] = data['Asset volume'].astype('float')
-data['Number of trades'] = data['Number of trades'].astype('float')
-data['Taker volume'] = data['Taker volume'].astype('float')
-data['Maker volume'] = data['Maker volume'].astype('float')
-data['nothing'] = data['nothing'].astype('float')
 
-data = data.set_index('Datetime')
+ma = tti.indicators.MovingAverage(input_data=df, period=20, ma_type='simple')
+bb = tti.indicators.BollingerBands(input_data=df, period=20, std_number=2, fill_missing_values=True)
+rsi = tti.indicators.RelativeStrengthIndex(input_data=df, period=14, fill_missing_values=True)
 
-ma = tti.indicators.MovingAverage(input_data=data, period=20, ma_type='simple')
-bb = tti.indicators.BollingerBands(input_data=data, period=20, std_number=2, fill_missing_values=True)
-rsi = tti.indicators.RelativeStrengthIndex(input_data=data, period=14, fill_missing_values=True)
+##run(saldo, ma, bb, rsi)
 
-print(ma.getTiData())
-print(bb.getTiData())
+print(saldo)
+print(rsi.getTiData())
+print(rsi.getTiGraph().show())
 
